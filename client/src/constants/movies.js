bfe65fc3e11652271bcc6a77dabb16800b3f29d5
Adil-Ansa12i <adil.ansari@byjus.com>
2021-11-02 14:45:57 +0530

    Pushing the code of Movies App

---
+++ client/src/constants/movies.js
@@ -0,0 +1,66 @@
+const movies = [{
+    id: 1,
+    title: "Ironman",
+    poster: "",
+    rating: 10
+},{
+    id: 2,
+    title: "Superman",
+    poster: "",
+    rating: 9
+},{
+    id: 3,
+    title: "Spiderman",
+    poster: "",
+    rating: 7
+},{
+    id: 4,
+    title: "Batman",
+    poster: "",
+    rating: 10
+},{
+    id: 5,
+    title: "Aquaman",
+    poster: "",
+    rating: 10
+},{
+    id: 6,
+    title: "Antman",
+    poster: "",
+    rating: 5
+
+},{
+    id: 7,
+    title: "spiderman2",
+    poster: "",
+    rating: 6
+},{
+    id: 8,
+    title: "Harry Potter 1",
+    poster: "",
+    rating: 10
+},{
+    id: 9,
+    title: "Harry Potter 2",
+    poster: "",
+    rating: 9
+},{
+    id: 10,
+    title: "Harry Potter 3",
+    poster: "",
+    rating: 8
+},{
+    id: 11,
+    title: "Harry Potter 4",
+    poster: "",
+    rating: 7
+},{
+    id: 12,
+    title: "Harry Potter 5",
+    poster: "",
+    rating: 10
+}]
+
+export {
+    movies
+}
